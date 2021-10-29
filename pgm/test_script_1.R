9f3e3f5c6f5c14d5798cdfd56af1f49f6984685f
Jason Zhang <jason.zhang@sdponcology.com>
2021-10-29 16:12:06 -0400

    Add Test Wrapper file and modify modularize test_script_1 and test_script_2.

--- pgm/test_script_1.R
+++ pgm/test_script_1.R
@@ -17,76 +17,75 @@
 ##   
 ##
 ## ---------------------------
-
-
-
-
-# Prepare content of demographic dataset
-STUDYID <- c("ABC-123", "ABC-123", "ABC-123")
-USUBJID <- c("ABC-123-0001", "ABC-123-0002", "ABC-123-0003")
-SUBJID <- c("0001", "0002", "0003")
-RFSTDTC <-
-  c(as.Date("2021-10-12"),
-    as.Date("2021-10-12"),
-    as.Date("2021-10-12"))
-RFENDTC <-
-  c(as.Date("2021-10-12"),
-    as.Date("2021-10-12"),
-    as.Date("2021-10-12"))
-RFXSTDTC <-
-  c(as.Date("2021-10-12"),
-    as.Date("2021-10-12"),
-    as.Date("2021-10-12"))
-RFXENDTC <-
-  c(as.Date("2021-10-12"),
-    as.Date("2021-10-12"),
-    as.Date("2021-10-12"))
-RFICDTC <-
-  c(as.Date("2021-10-12"),
-    as.Date("2021-10-12"),
-    as.Date("2021-10-12"))
-RFPENDTC <- c(NA, NA, NA)
-DTHDTC <- c(NA, NA, NA)
-DTHFL <- c(NA, NA, NA)
-SITEID <- c("123", "123", "123")
-BRTHDTC <-
-  c(as.Date("2000-10-12"),
-    as.Date("2000-10-12"),
-    as.Date("2000-10-12"))
-AGE <- c(21, 21, 21)
-SEX <- c("M", "F", "M")
-RACE <- c("White", "Asian", "White")
-ETHNIC <-
-  c("NOT HISPANIC OR LATINO",
-    "NOT HISPANIC OR LATINO",
-    "NOT HISPANIC OR LATINO")
-ARMCD <- c("A", "A", "B")
-ARM <- c("Arm A", "Arm A", "Arm B")
-ACTARMCD <- c("A", "A", "B")
-ACTARM <- c("Arm A", "Arm A", "Arm B")
-COUNTRY <- c("USA", "USA", "USA")
-
-# Creating demographic dataset
-dm <-
-  data.frame(STUDYID,
-             USUBJID,
-             SUBJID,
-             RFSTDTC,
-             RFENDTC,
-             RFXSTDTC,
-             RFXENDTC,
-             RFICDTC,
-             RFPENDTC,
-             DTHDTC,
-             DTHFL,
-             SITEID,
-             BRTHDTC,
-             AGE,
-             SEX,
-             RACE,
-             ETHNIC,
-             ARMCD,
-             ARM,
-             ACTARMCD,
-             ACTARM,
-             COUNTRY)
+test_script_1 <- function(){
+  # Prepare content of demographic dataset
+  STUDYID <- c("ABC-123", "ABC-123", "ABC-123")
+  USUBJID <- c("ABC-123-0001", "ABC-123-0002", "ABC-123-0003")
+  SUBJID <- c("0001", "0002", "0003")
+  RFSTDTC <-
+    c(as.Date("2021-10-12"),
+      as.Date("2021-10-12"),
+      as.Date("2021-10-12"))
+  RFENDTC <-
+    c(as.Date("2021-10-12"),
+      as.Date("2021-10-12"),
+      as.Date("2021-10-12"))
+  RFXSTDTC <-
+    c(as.Date("2021-10-12"),
+      as.Date("2021-10-12"),
+      as.Date("2021-10-12"))
+  RFXENDTC <-
+    c(as.Date("2021-10-12"),
+      as.Date("2021-10-12"),
+      as.Date("2021-10-12"))
+  RFICDTC <-
+    c(as.Date("2021-10-12"),
+      as.Date("2021-10-12"),
+      as.Date("2021-10-12"))
+  RFPENDTC <- c(NA, NA, NA)
+  DTHDTC <- c(NA, NA, NA)
+  DTHFL <- c(NA, NA, NA)
+  SITEID <- c("123", "123", "123")
+  BRTHDTC <-
+    c(as.Date("2000-10-12"),
+      as.Date("2000-10-12"),
+      as.Date("2000-10-12"))
+  AGE <- c(21, 21, 21)
+  SEX <- c("M", "F", "M")
+  RACE <- c("White", "Asian", "White")
+  ETHNIC <-
+    c("NOT HISPANIC OR LATINO",
+      "NOT HISPANIC OR LATINO",
+      "NOT HISPANIC OR LATINO")
+  ARMCD <- c("A", "A", "B")
+  ARM <- c("Arm A", "Arm A", "Arm B")
+  ACTARMCD <- c("A", "A", "B")
+  ACTARM <- c("Arm A", "Arm A", "Arm B")
+  COUNTRY <- c("USA", "USA", "USA")
+  
+  # Creating demographic dataset
+  dm <-
+    data.frame(STUDYID,
+               USUBJID,
+               SUBJID,
+               RFSTDTC,
+               RFENDTC,
+               RFXSTDTC,
+               RFXENDTC,
+               RFICDTC,
+               RFPENDTC,
+               DTHDTC,
+               DTHFL,
+               SITEID,
+               BRTHDTC,
+               AGE,
+               SEX,
+               RACE,
+               ETHNIC,
+               ARMCD,
+               ARM,
+               ACTARMCD,
+               ACTARM,
+               COUNTRY)
+  return(dm)
+}
--- pgm/test_script_2.R
+++ pgm/test_script_2.R
@@ -17,94 +17,97 @@
 ##   
 ##
 ## ---------------------------
-
-library(ggplot2)
 # Prepare content of demographic dataset
-STUDYID <- c("ABC-123", "ABC-123", "ABC-123")
-USUBJID <- c("ABC-123-0001", "ABC-123-0002", "ABC-123-0003")
-SUBJID <- c("0001", "0002", "0003")
-RFSTDTC <-
-  c(as.Date("2021-10-12"),
-    as.Date("2021-10-12"),
-    as.Date("2021-10-12"))
-RFENDTC <-
-  c(as.Date("2021-10-12"),
-    as.Date("2021-10-12"),
-    as.Date("2021-10-12"))
-RFXSTDTC <-
-  c(as.Date("2021-10-12"),
-    as.Date("2021-10-12"),
-    as.Date("2021-10-12"))
-RFXENDTC <-
-  c(as.Date("2021-10-12"),
-    as.Date("2021-10-12"),
-    as.Date("2021-10-12"))
-RFICDTC <-
-  c(as.Date("2021-10-12"),
-    as.Date("2021-10-12"),
-    as.Date("2021-10-12"))
-RFPENDTC <- c(NA, NA, NA)
-DTHDTC <- c(NA, NA, NA)
-DTHFL <- c(NA, NA, NA)
-SITEID <- c("123", "123", "123")
-BRTHDTC <-
-  c(as.Date("2000-10-12"),
-    as.Date("2000-10-12"),
-    as.Date("2000-10-12"))
-AGE <- c(21, 21, 21)
-SEX <- c("M", "F", "M")
-RACE <- c("White", "Asian", "White")
-ETHNIC <-
-  c("NOT HISPANIC OR LATINO",
-    "NOT HISPANIC OR LATINO",
-    "HISPANIC OR LATINO")
-ARMCD <- c("A", "A", "B")
-ARM <- c("Arm A", "Arm A", "Arm B")
-ACTARMCD <- c("A", "A", "B")
-ACTARM <- c("Arm A", "Arm A", "Arm B")
-COUNTRY <- c("USA", "USA", "USA")
+test_script_2 <- function(){
+  STUDYID <- c("ABC-123", "ABC-123", "ABC-123")
+  USUBJID <- c("ABC-123-0001", "ABC-123-0002", "ABC-123-0003")
+  SUBJID <- c("0001", "0002", "0003")
+  RFSTDTC <-
+    c(as.Date("2021-10-12"),
+      as.Date("2021-10-12"),
+      as.Date("2021-10-12"))
+  RFENDTC <-
+    c(as.Date("2021-10-12"),
+      as.Date("2021-10-12"),
+      as.Date("2021-10-12"))
+  RFXSTDTC <-
+    c(as.Date("2021-10-12"),
+      as.Date("2021-10-12"),
+      as.Date("2021-10-12"))
+  RFXENDTC <-
+    c(as.Date("2021-10-12"),
+      as.Date("2021-10-12"),
+      as.Date("2021-10-12"))
+  RFICDTC <-
+    c(as.Date("2021-10-12"),
+      as.Date("2021-10-12"),
+      as.Date("2021-10-12"))
+  RFPENDTC <- c(NA, NA, NA)
+  DTHDTC <- c(NA, NA, NA)
+  DTHFL <- c(NA, NA, NA)
+  SITEID <- c("123", "123", "123")
+  BRTHDTC <-
+    c(as.Date("2000-10-12"),
+      as.Date("2000-10-12"),
+      as.Date("2000-10-12"))
+  AGE <- c(21, 21, 21)
+  SEX <- c("M", "F", "M")
+  RACE <- c("White", "Asian", "White")
+  ETHNIC <-
+    c("NOT HISPANIC OR LATINO",
+      "NOT HISPANIC OR LATINO",
+      "HISPANIC OR LATINO")
+  ARMCD <- c("A", "A", "B")
+  ARM <- c("Arm A", "Arm A", "Arm B")
+  ACTARMCD <- c("A", "A", "B")
+  ACTARM <- c("Arm A", "Arm A", "Arm B")
+  COUNTRY <- c("USA", "USA", "USA")
+  
+  # Creating demographic dataset
+  dm <-
+    data.frame(
+      STUDYID,
+      USUBJID,
+      SUBJID,
+      RFSTDTC,
+      RFENDTC,
+      RFXSTDTC,
+      RFXENDTC,
+      RFICDTC,
+      RFPENDTC,
+      DTHDTC,
+      DTHFL,
+      SITEID,
+      BRTHDTC,
+      AGE,
+      SEX,
+      RACE,
+      ETHNIC,
+      ARMCD,
+      ARM,
+      ACTARMCD,
+      ACTARM,
+      COUNTRY
+    )
+  
+  p <- ggplot(data = dm, mapping = aes(x = RACE, fill = SEX)) +
+    geom_bar(stat = "count") +
+    scale_fill_discrete(c("red", "blue")) +
+    theme_bw() + facet_grid(cols = vars(ETHNIC)) +
+    labs(
+      x = "Frequency",
+      y = "Race",
+      title = "Race Histogram",
+      subtitle = "N = (3)",
+      caption = "Protocol ID: ABC-123"
+    ) + 
+    theme(
+      axis.text.x = element_text(angle=45, vjust=1, hjust=1),
+      legend.position = "bottom",
+      plot.title = element_text(size = 12, face = "bold", hjust = 0.5),
+      plot.subtitle = element_text(hjust = 0.5))
+  
+    return(p)
+}
 
-# Creating demographic dataset
-dm <-
-  data.frame(
-    STUDYID,
-    USUBJID,
-    SUBJID,
-    RFSTDTC,
-    RFENDTC,
-    RFXSTDTC,
-    RFXENDTC,
-    RFICDTC,
-    RFPENDTC,
-    DTHDTC,
-    DTHFL,
-    SITEID,
-    BRTHDTC,
-    AGE,
-    SEX,
-    RACE,
-    ETHNIC,
-    ARMCD,
-    ARM,
-    ACTARMCD,
-    ACTARM,
-    COUNTRY
-  )
-
-ggplot(data = dm, mapping = aes(x = RACE, fill = SEX)) +
-  geom_bar(stat = "count") +
-  scale_fill_discrete(c("red", "blue")) +
-  theme_bw() + facet_grid(cols = vars(ETHNIC)) +
-  labs(
-    x = "Frequency",
-    y = "Race",
-    title = "Race Histogram",
-    subtitle = "N = (3)",
-    caption = "Protocol ID: ABC-123"
-  ) + 
-  theme(
-    axis.text.x = element_text(angle=45, vjust=1, hjust=1),
-    legend.position = "bottom",
-    plot.title = element_text(size = 12, face = "bold", hjust = 0.5),
-    plot.subtitle = element_text(hjust = 0.5)
     
